83bfa31df63910d865e3cddd1219c7852b336026
Adam Carheden <adam.carheden@gmail.com>
2016-11-07 06:19:49 -0700

    Compiles. Loads. Doesn't throw errors on input

--- src/html-input-tools.js
+++ src/html-input-tools.js
@@ -1,5 +1,3 @@
-"use strict"
-
 /*
 ## User input process:
 1. User types something
@@ -54,25 +52,18 @@
 
 */
 
-var cursorLocation(tgt) {
-	if (tgt.selectionDirection === 'backward') {
-	} else { // For 'none', we assume forward
-		
-	}
-}
-
 // Functions the user should implement
 const user = {
 	// Should strip formatting applied by the format() function and return the stripped value
-	unformat: function(value) {},
+	unformat: function(value) { return value },
 
 	// Should:
 	// * return true if the value is valid
 	// * return false if the value is invalid but an acceptable intermediate value
-	// * throw an invalidValue exception if the value is invalid and should be prevented if possible
+	// * throw an InvalidValueError if the value is invalid and should be prevented if possible
 	// Params:
 	// * value - The value of the input field with formatting stripped by unformat()
-	validate: function(value) {},
+	validate: function(value) { return true },
 
 	// Params:
 	// * value - The validated, unformatted value of the input field
@@ -83,93 +74,70 @@ const user = {
 	//   * value: same as above
 	//   * cursorPos: The offset in the formatted string of the cursor
   //     This is optional, but without it the cursor will be moved to the end of the input with each keystroke
-	format: function(value) {},
+	format: function(value, cursorPos) { return {value: value, cursorPos: cursorPos } },
 
 	// Should assign the value to backend storage, optionally validating with respect to other related data.
-	// If the new value would put the backend in an invalid state, it should /not/ be assigned and instead an
-	// 'cantSync' exception should be thrown
+	// If the new value would put the backend in an invalid state, it should /not/ be assigned and instead
+	// it should throw a CantSyncError
 	// Params:
 	// * value - The validated, unformatted value of the input field
-	synchronize: function(value) {},
+	synchronize: function(value) { },
 }
 
-const exceptions = {
-	// Throw in sychronize() if assigning the value to the backend storage would put it into an invalid state
-	cantSync: {},
-
-	// Throw in validate() if value is invalid and input should be blocked if possible
-	invalidValue: {},
+class CantSyncError extends Error {}
+class InvalidValueError extends Error {}
+const errors = {
+	CantSyncError: CantSyncError,
+	InvalidValueError: InvalidValueError
 }
 
 // Events we emit
-const callbacks = {
+const emit = {
 	// Called when the input changes and the value is invalid
 	// We prevent this when possible, but we can't stop the user from pasting it in
-	invalid: {}
+	invalid: function() {},
 
 	// Called when the input changes and the value is invalid but allowed (intermediate)
-	intermediate: {}
+	intermediate: function() {},
 
 	// Called when the input changes and the value is valid
-	valid: {}
+	valid: function() {},
 
 	// Called when the backend storage update fails
-	unsynced: {}
+	unsynced: function() {},
 
 	// Called when the backend storage update is successful
-	synced: {}
+	synced: function() {},
 }
 
-var getCursorPos(target) {
-	// TODO
-	return 0;
-}
-var doValidate(v, cursorPos, e) {
-	if (!cursorPos) cursorPos = getCursorPos(e.target);
-	var result;
-	try {
-		result = user.validate(v, cursorPos);
-		if (result) {
-			callbacks.invalid(v);
-		} else {
-			callbacks.valid(v);
-		}
-		var fmtd = callback.format(v, cursorPos);
-		if (typeof(fmtd) == 'string') {
-			fmtd = { cursorPos: false, value: fmtd, };
-		}
-		e.target.value = fmtd.value;
-		// TODO: set new cursorPos (if possible)
-	} catch (ex) {
-		callbacks.invalid(v, e.target.value);
-		return undefined;
-	}
-	return result;
-}
+
 
 // Events we intercept
 const events = {
-	intput:
-		keydown: function(e) {},
-		keyup: function(e) {},
-		keypress: function(e) {
-			e.preventDefault();
-			var v = user.unformat(e.target.value);
-			var cursorPos = getCursorPos(e.target);
-			// TODO get cursorPos from e.target.selection*
-			// TODO - munge in key at cursor position
-			doValidate(v, cursorPos, e);
+	HTMLInputElement: {
+		keydown: function(e) {
+			if (this.ignoreKey(e)) return true
 		},
+		keyup: function(e) {
+			if (this.ignoreKey(e)) return true
+		},
+		keypress: function(e) {
+			if (this.ignoreKey(e)) return true
+			console.log('keypress')
+			if (this.validateAndFormat(e)) e.preventDefault()
+		},
+		// keypress handles normal typing and prevents input from firing
+		// input handles things when the user copyies and pasts something in or deletes with ctrl+x or something
 		input: function(e) { 
-			var v = user.unformat(e.target.value);
-			doValidate(v, false, e); 
+			console.log('input')
+			if (this.validateAndFormat(e)) e.preventDefault()
 		},
 		change: function(e) {
 			try {
-				user.synchronize(this._value);
-				callbacks.synced(value);
+				user.synchronize(this._value)
+				emit.synced(this._value)
 			} catch (ex) {
-				callbacks.unsynched(ex);
+				emit.unsynched(ex)
 			}
 		},
 	}
@@ -177,5 +145,91 @@ const events = {
 	// TODO: select, checkbox, button (maybe?)
 }
 
+var defaultOpts = {
+	ignoreCtrl: true,
+	ignoreAlt: true,
+}
+
+class ManagedInput {
+	constructor(input, callbacks, opts) {
+		if (typeof input === 'string') {
+			this.input = document.getElementById(input)
+			if (!this.input) {
+				this.input = document.querySelector(input)
+				if (!this.input) throw new Error(`Failed to find element by id or query selector '${input}'`)
+			}
+		} else {
+			this.input = input
+		}
+		if (!(typeof this.input === 'object')) {
+		}
+		if (!(this.input instanceof HTMLInputElement)) { // eslint-disable-line no-undef
+			throw Error(`Expected an HTMLInputElement, got a ${this.input.constructor.name}`)
+		}
+
+		this.callbacks = {}
+		if (typeof callbacks === 'undefined') callbacks = {}
+		if (typeof callbacks !== 'object') throw new Error(`callbacks should be an object, not a ${typeof opts}`)
+		Object.keys(user).forEach((cb) => {
+			this.callbacks[cb] = (cb in callbacks) ? callbacks[cb] : user[cb]
+		})
+
+		this.opts = {}
+		if (typeof opts === 'undefined') opts = {}
+		if (typeof opts !== 'object') throw new Error(`opts should be an object, not a ${typeof opts}`)
+		Object.keys(defaultOpts).forEach((opt) => {
+			this.opts[opt] = (opt in opts) ? opts[opt] : defaultOpts[opt]
+		})
+
+		this.inputType = this.input.constructor.name
+		Object.keys(events[this.inputType]).forEach((evnt) => {
+			this.input.addEventListener(evnt, (e) => { events[this.inputType][evnt].bind(this)(e) })
+		})
+	}
+
+	ignoreKey(evnt) {
+		if (this.opts.ignoreCtrl && evnt.ctrlKey) return true
+		if (this.opts.ignoreAlt && evnt.altKey) return true
+	}
+
+	// If current state of input is a valid value,
+	// apply formatting.
+	// Call the valid/invalid callbacks in any case
+	// Returns:
+	// 	true  - calling event should prevent user input if possible ()
+	// 	false - calling event should allow user input
+	validateAndFormat(e) {
+		var oldValue = this.input.value
+
+		var beforeSelection = e.target.value.slice(0, e.target.selectionStart)
+		var afterSelection = e.target.value.slice(e.target.selectionEnd,-1)
+		var newValue = `${beforeSelection}${e.key}${afterSelection}`
+
+		var unformatted = this.callbacks.unformat(newValue, e.target.selectionStart)
+		if (typeof unformatted !== 'object') unformatted = { value: unformatted, cursorPos: false }
+		try {
+			if (!this.callbacks.validate(unformatted.value)) {
+				emit.invalid(oldValue, newValue, unformatted.value, this.input.value)
+			} else {
+				var formatted = emit.format(unformatted.value, unformatted.cursorPos)
+				if (typeof formatted !== 'object') formatted = { value: formatted, cursorPos: false }
+				e.target.value = formatted.value
+				if (formatted.cursorPos) {
+					this.input.setSelectionRange(formatted.cursorPos, formatted.cursorPos+1)
+				}
+				emit.valid(oldValue, newValue, unformatted.value, this.input.value)
+			}
+		} catch (ex) {
+			emit.invalid(oldValue, newValue, unformatted.value, this.input.value)
+			return false
+		}
+		return true
+	}
+
+}
+
 module.exports = {
-}e
+	ManagedInput: ManagedInput,
+	events: events,
+	errors: errors,
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require( 'webpack' )
+const path = require('path')
+const projectRoot = path.resolve(__dirname)
+
+module.exports = {
+  entry: './src/html-input-tools.js',
+  output: {
+    path: projectRoot,
+    filename: 'html-input-tools.js',
+    library: 'html-input-tools',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: [ '', '.js']
+  },
+  module: {
+		preLoaders: [
+			{
+				test: /.js$/,
+				loader: 'eslint',
+				include: projectRoot,
+				exclude: /node_modules/
+			}
+		],
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+	/*
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin( {
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    } )
+  ]
+	*/
+}
